a3838a0f0816071189f65779641d39da01ec337b
Pratik <survasepratik1304@gmail.com>
2026-01-15 22:14:48 +0530

    better auth setup

---
+++ server/src/lib/auth.ts
@@ -0,0 +1,10 @@
+import prisma from "./db.js";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+});
