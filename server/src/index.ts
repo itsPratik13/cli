81bc5ff4c877b4e41d81bb1831675cdfdc9afc15
Pratik <survasepratik1304@gmail.com>
2026-01-16 22:46:13 +0530

    better auth done

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,21 +1,32 @@
 import express from "express";
 import cors from "cors";
-import helmet from "helmet";
-import morgan from "morgan";
 import dotenv from "dotenv";
+import { toNodeHandler,fromNodeHeaders } from "better-auth/node";
+import { auth } from "./lib/auth.js";
+
 
 dotenv.config();
 
 const app = express();
-
-app.use(cors());
-app.use(helmet());
-app.use(morgan("dev"));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+app.all("/api/auth/*splat", toNodeHandler(auth));
 app.use(express.json());
+app.get("/api/me", async (req, res) => {
+  const session = await auth.api.getSession({
+    headers: fromNodeHeaders(req.headers),
+  });
+  return res.json(session);
+});
 
 app.get("/", (req, res) => {
   res.send("Server is running ");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT} `));
